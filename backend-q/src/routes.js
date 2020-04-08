602399a3e1b94ca2b0aebe7aca2f1b28eb419839
arkeyt <arkeyt>
2020-04-07 22:23:02 -0300

    commit aula 5 semana omnistack

---
+++ backend-q/src/routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const {celebrate, Segments, Joi } = require('celebrate');
+
+const OngsController = require('./controllers/OngsController');
+const IncidentController = require('./controllers/IncidentController'); 
+const ProfileController = require('./controllers/ProfileController'); 
+const SessionController = require('./controllers/SessionController'); 
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+routes.get('/ongs', OngsController.index);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    }),
+}), OngsController.create);
+
+routes.post('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }),
+}),IncidentController.create);
+
+routes.get('/incidents', IncidentController.index);
+
+routes.delete('/incidents/:id' ,celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id : Joi.number().required(),
+    }),
+}) ,IncidentController.delete);
+
+
+module.exports = routes;
+
